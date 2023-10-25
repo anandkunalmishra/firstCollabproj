efc91af7628565f78332bff6dd419abaea992a52
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2023-10-26 01:24:41 +0530

    added font to index.html

---
+++ src/component/rightSection/Rightsection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Rightsection.css"
+
+function Rightsection() {
+  return (
+    <div className='rightsection'>
+        
+    </div>
+  )
+}
+
+export default Rightsection
